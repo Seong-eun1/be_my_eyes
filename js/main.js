c68f2a8af999712ae5932d7a6c14f1571d56a242
한승민 <gksckdfhr99@naver.com>
2022-07-26 15:08:26 +0900

    update code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const selectLang = document.getElementById("select-lang")
+const text = document.getElementById("text")
+const btnRead = document.getElementById("btn-read")
+
+document.addEventListener("DOMContentLoaded", function(){
+  btn_click();
+})
+
 function call() {
   text.value = '코카콜라 컴퍼니에서 생산하는 콜라입니다.\n캔음료이며 제로 콜라는 아닙니다.\n유통기한은 7월 30일까지입니다.';
 
@@ -32,15 +40,12 @@ function speak(text, opt_prop) {
 // 이벤트 영역
 //const selectLang = "ko-KR" //document.getElementById("select-lang")
 
-const selectLang = document.getElementById("select-lang")
-const text = document.getElementById("text")
-const btnRead = document.getElementById("btn-read")
-
-
-btnRead.addEventListener("click", e => {
-    speak(text.value, {
-        rate: 1,
-        pitch: 1.2,
-        lang: selectLang.options[selectLang.selectedIndex].value
-    })
-})
+function btn_click(){
+  btnRead.addEventListener("click", e => {
+      speak(text.value, {
+          rate: 1,
+          pitch: 1.2,
+          lang: selectLang.options[selectLang.selectedIndex].value
+      })
+  })
+}
